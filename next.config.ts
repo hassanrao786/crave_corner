ec635d9b77a5d3731ec11ec2a0c7a1b2946264e7
Muhammad Hassan <hassanrao875@gmail.com>
2025-01-15 11:14:51 +0500

    update

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,12 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import type { NextConfig } from "next";
 
-// Set up the file paths for dynamic imports (ESLint config)
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const nextConfig: NextConfig = {
+  /* config options here */
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+  
 
-// Extend your ESLint config with Next.js core-vitals and TypeScript
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  }
+  
 
-// Your Next.js config
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,  // Skip ESLint checks during build
-  },
-};
 
 export default nextConfig;
